5cb75e54a270af62ae0c92a9821e853694db91c7
mageshkannan <mageshkannanam@gmail.com>
2025-07-19 19:54:01 +0530

    add: copy array to new array

---
+++ basics/CopyArray.java
@@ -0,0 +1,14 @@
+package basics;
+
+import java.util.Arrays;
+
+public class CopyArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int[] newArr = new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+            newArr[i]=arr[i];
+        }
+        System.out.println(Arrays.toString(newArr));
+    }
+}
